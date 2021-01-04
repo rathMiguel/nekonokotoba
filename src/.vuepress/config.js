c4f613583f18fca9350f9ddd868838f08ab43b7d
rath_miguel <rath.miguel@gmail.com>
2021-01-04 23:51:03 +0900

    delete push-dirパッケージの削除

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,5 +3,6 @@ module.exports = {
     ['meta', {'http-equiv': 'X-UA-Compatible', content: 'IE=edge'}],
     ['meta', { name: 'robots', content: 'noindex' }]
   ],
-  title: "猫のことば　メモ置き場"
+  title: "猫のことば　メモ置き場",
+  plugins: ['@vuepress/medium-zoom']
 }
