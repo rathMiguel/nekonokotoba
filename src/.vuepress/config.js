409ec2272792b71aff0070f5e4accc1d50d14ca8
rath_miguel <rath.miguel@gmail.com>
2021-01-08 19:49:48 +0900

    add, move よわーざのメモ追加　攻略用のページの追加

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -6,5 +6,14 @@ module.exports = {
     ['meta', { name: 'robots', content: 'noindex' }]
   ],
   title: "猫のことば　メモ置き場",
-  plugins: ['@vuepress/medium-zoom']
+  plugins: ['@vuepress/medium-zoom'],
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/', items: [
+        { text: '深淵の王墓', link: '/guide/royaltombs_of_abyss/' },
+        { text: '深淵の古城', link: '/guide/old_castle_of_abyss/' }
+      ]}
+    ],
+  }
 }
