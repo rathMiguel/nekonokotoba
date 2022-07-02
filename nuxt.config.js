55b7ed54696c40e677cb4833d47f59a8c3b4bbee
rath_miguel <rath.miguel@gmail.com>
2022-07-02 12:50:04 +0900

    add: pwaの追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,21 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/iphone5_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/iphone6_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)', href: '/splashscreens/iphoneplus_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)', href: '/splashscreens/iphonex_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/iphonexr_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)', href: '/splashscreens/iphonexsmax_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/ipad_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/ipadpro1_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/ipadpro3_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)', href: '/splashscreens/ipadpro2_splash.png' },
+      { name: 'mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+      { name: 'apple-mobile-web-app-title', content: '猫のことば研究所' }
     ],
     css: [
       {src: '~/assets/scss/common.scss'}
@@ -42,7 +56,8 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     // '@nuxtjs/eslint-module',
-    ['@nuxtjs/fontawesome', { component: 'fontAwesome', suffix: true }]
+    ['@nuxtjs/fontawesome', { component: 'fontAwesome', suffix: true }],
+    '@nuxtjs/pwa'
   ],
 
   webfontloader: {
@@ -50,6 +65,32 @@ export default {
       families: [
         'Noto+Sans+JP:400,700',
         'Karla'
+      ]
+    }
+  },
+
+  pwa: {
+    icon: {
+      source: 'icon_pwa.png',
+      fileName: 'icon_pwa.png'
+    },
+    manifest: {
+      lang: 'ja',
+      name: '猫のことば研究所',
+      short_name: '猫のことば研究所',
+      description: 'ラグナロクオンライン内で集めたデータやちょっと便利かもしれないツール等を公開しています。',
+      display: 'standalone',
+      theme_color: '#25374B',
+      background_color: '#E0D9BC',
+      orientation: 'portrait',
+      scope: '/',
+      start_url: '/',
+      icons: [
+        {
+          src: 'icon_pwa.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
       ]
     }
   },
