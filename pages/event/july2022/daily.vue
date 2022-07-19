d1abc71f9cc967a10493a89f73939f5e46582cfc
rath_miguel <rath.miguel@gmail.com>
2022-07-20 06:29:38 +0900

    update: 7/20納品納品データ

--- pages/event/july2022/daily.vue
+++ pages/event/july2022/daily.vue
@@ -34,6 +34,49 @@ export default {
       },
       dailyData: [
         {
+          date: '2022-07-20',
+          lookingfor: [
+            {
+              level: 'Easy',
+              map: '202207m021map.jpg',
+              detail: '202207m021detail.jpg',
+              caption: 'ゲフェン建物内　北部屋から2F'
+            },
+            {
+              level: 'Hard',
+              map: '202207m022map.jpg',
+              detail: '202207m022detail.jpg',
+              caption: '蟻地獄ダンジョン　2F'
+            }
+          ],
+          delivary: [
+            {
+              level: 'Easy',
+              item_id: 1020,
+              item_name: '長い髪',
+              amount: 25
+            },
+            {
+              level: 'Normal',
+              item_id: 7345,
+              item_name: '囚人の腕輪',
+              amount: 60
+            },
+            {
+              level: 'Hard',
+              item_id: 2572,
+              item_name: '美味しい果汁',
+              amount: 15
+            },
+            {
+              level: 'Inferno',
+              item_id: 25618,
+              item_name: 'ゴシック風の陶磁器人形',
+              amount: 2
+            }
+          ]
+        },
+        {
           date: '2022-07-19',
           lookingfor: [
             {
@@ -54,25 +97,25 @@ export default {
               level: 'Easy',
               item_id: 928,
               item_name: '虫の觸角',
-              amount: '50'
+              amount: 50
             },
             {
               level: 'Normal',
               item_id: 7119,
               item_name: '菌糸',
-              amount: '10'
+              amount: 10
             },
             {
               level: 'Hard',
               item_id: 25258,
               item_name: '壊れた矢',
-              amount: '30'
+              amount: 30
             },
             {
               level: 'Inferno',
               item_id: 25262,
               item_name: '乾燥したクローバー',
-              amount: '1'
+              amount: 1
             }
           ]
         }
