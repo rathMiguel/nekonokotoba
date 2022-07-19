ba6edb1ea4564032953bb21be5e30dcc0304d223
rath_miguel <rath.miguel@gmail.com>
2022-07-19 15:21:29 +0900

    update: アイキャッチ、metaの追加

--- pages/event/july2022/index.vue
+++ pages/event/july2022/index.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 div
   MainHeader(h1='ゲフェンメロンフェスタ優品 イベント情報')
+  ContentEyecatch(name="july2022.png")
   .main-content
     h2 特設サイト
     p: a(href="https://ragnarokonline.gungho.jp/special/melon-fes2022/" target="_blank") ゲフェン・メロンフェスタ ～深淵からの贈り物 優品～ | ラグナロクオンライン公式サイト
@@ -13,3 +14,22 @@ div
     h2 レシピ/材料一覧
     nuxt-link(to="/event/july2022/recipe/") レシピ・材料一覧
 </template>
+
+<script>
+import Meta from '~/plugins/meta'
+
+export default {
+  mixins: [ Meta ],
+  data() {
+    return {
+      meta: {
+        title: `ゲフェンメロンフェスタ優品 イベントまとめ - ${process.env.BASETITLE}`,
+        description: '',
+        type: 'website',
+        url: `${process.env.BASEURL}/`,
+        image: `${process.env.BASEURL}/ogp.png`
+      }
+    }
+  }
+}
+</script>
