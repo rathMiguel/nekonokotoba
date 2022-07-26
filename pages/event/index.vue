c540fb43c44bbbe04333d93b58ac2f61245a2032
rath_miguel <rath.miguel@gmail.com>
2022-07-26 19:44:54 +0900

    update: 文章の修正

--- pages/event/index.vue
+++ pages/event/index.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
 div
-  MainHeader(h1="シーズンイベント　まとめ")
+  MainHeader(h1="過去のシーズンイベント一覧")
   ContentEyecatch(name="transport.jpg")
   .main-content
     article.article-blog
       p 期間限定イベントの情報をまとめています
       ul
-        li
+        //- li
           nuxt-link(to="/event/july2022/") ゲフェンメロンフェスタ　優品
           ul
             li: nuxt-link(to="/event/july2022/daily/") 納品 / 迷子探し
@@ -24,8 +24,8 @@ export default {
   data() {
     return {
       meta: {
-        title: `シーズンイベント - ${process.env.BASETITLE}`,
-        description: 'シーズンイベントの情報をまとめています',
+        title: `過去のシーズンイベント一覧 - ${process.env.BASETITLE}`,
+        description: '過去に開催されたのシーズンイベント一覧です',
         type: 'website',
         url: `${process.env.BASEURL}/event/transport/`,
         image: `${process.env.BASEURL}/ogp.png`
